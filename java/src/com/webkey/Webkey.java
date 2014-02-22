45b7ac4d07b7923d462e7caa1831e1f27afc79bf
Dimitri Eberhardt Prado <dprado@e3c.com.br>
2014-02-22 15:39:38 -0300

    Remove RemoteScreen and Chat functionality.

--- java/src/com/webkey/Webkey.java
+++ java/src/com/webkey/Webkey.java
@@ -12,9 +12,10 @@ import android.widget.TabHost;
 
 public class Webkey extends TabActivity {
 	TabHost tabHost;
-	
+
     /** Called when the activity is first created. */
-	public void onCreate(Bundle savedInstanceState) {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.main);
 
@@ -23,45 +24,25 @@ public class Webkey extends TabActivity {
 	    TabHost.TabSpec spec;  // Resusable TabSpec for each tab
 	    Intent intent;  // Reusable Intent for each tab
 
-	    
+
 	    // Create an Intent to launch an Activity for the tab (to be reused)
 	    intent = new Intent().setClass(this, com.webkey.activity.WebkeyTab.class);
 	    spec = tabHost.newTabSpec("control").setIndicator("Control",
 	                      res.getDrawable(R.drawable.ic_tab_control))
 	                  .setContent(intent);
 	    tabHost.addTab(spec);
-	    
+
 	    intent = new Intent().setClass(this, com.webkey.activity.SettingsTab.class);
 	    spec = tabHost.newTabSpec("settings").setIndicator("Settings",
 	                      res.getDrawable(R.drawable.ic_tab_settings))
 	                  .setContent(intent);
 	    tabHost.addTab(spec);
-	    
-
-	    intent = new Intent().setClass(this, com.webkey.activity.ChatUI.class);
-	    spec = tabHost.newTabSpec("chat").setIndicator("Chat",
-	                      res.getDrawable(R.drawable.ic_tab_chat))
-	                  .setContent(intent);
-	    tabHost.addTab(spec);
-
-            intent = new Intent().setClass(this, com.webkey.remotescreen.RemoteScreenActivity.class);
-            spec = tabHost.newTabSpec("remotescreen").setIndicator("RemoteScreen",res.getDrawable(R.drawable.ic_tab_remote))
-                                .setContent(intent);
-            tabHost.addTab(spec);
 
 	    tabHost.setCurrentTab(0);
 	}
-	
+
 	@Override
-	public void onResume(){		
+	public void onResume(){
 		super.onResume();
-		String isChatMsg=null;
-//        Log.d("Webkey_java: ","onresume");
-        Intent intent = getIntent();
-        isChatMsg = intent.getAction();
-        if(isChatMsg != null && isChatMsg.equals("chat")){
-//        	Log.d("Webkey_java | webkey: ","received intent tab parameter: "+isChatMsg);
-        	tabHost.setCurrentTab(2);
-        }
-	}		
+	}
 }
